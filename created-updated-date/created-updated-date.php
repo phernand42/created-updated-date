becfb35edfb8d4e4c983c645ba6de7b6425bc440
Paul Hernandez <phernand42@gmail.com>
2024-03-08 22:28:52 -0500

    Added some code comments and adjusted wrapper for dates

--- created-updated-date/created-updated-date.php
+++ created-updated-date/created-updated-date.php
@@ -1,13 +1,12 @@
 <?php
 /*
 Plugin Name: Created Updated Date
-Plugin URI: http://example.com/
-Description: Adds creation and modification dates to pages and posts, and includes meta tags for dates.
+Description: Adds creation and modification dates to pages and posts and includes meta tags for dates.
 Version: 1.0
 Author: Paul H
-Author URI: http://example.com/
 */
 
+//Add date meta tags to head
 function cud_add_dcterms_meta_tags() {
   if (is_single() || is_page()) {
     $post_id = get_queried_object_id();
@@ -21,19 +20,20 @@ function cud_add_dcterms_meta_tags() {
 
 add_action('wp_head', 'cud_add_dcterms_meta_tags');
 
+//Display create and modified dates at the bottom of post or page
 function cud_display_post_dates($content) {
   if (is_single() || is_page()) {
     $post_created = get_the_date('F j, Y');
     $post_modified = get_the_modified_date('F j, Y');
 
-    $date_info = '<p class="post-dates" style="font-style: italic;">';
+    $date_info = '<div class="created-modified-dates">';
     $date_info .= 'Created ' . $post_created;
 
     if ($post_created !== $post_modified) {
         $date_info .= ', Updated ' . $post_modified;
     }
 
-    $date_info .= '</p>';
+    $date_info .= '</div>';
 
     $content .= $date_info;
   }
